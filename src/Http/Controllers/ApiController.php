2862e9db8cec37cf04855ae21e4c03ba3ba33cd0
Edwin Rahmad Toha <edwin.rahmad.toha007@gmail.com>
2024-12-22 12:26:53 +0700

    initialize

---
+++ src/Http/Controllers/ApiController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Edwinrtoha\Laravelboilerplate\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ApiController extends Controller
+{
+    public function index()
+    {
+        return 'Hello from the LaravelBoilerplate package!';
+    }
+}
