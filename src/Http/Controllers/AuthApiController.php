1e993a6c0493709e55544d9d3a0fcee6bf074410
Edwin Rahmad Toha <edwin.rahmad.toha007@gmail.com>
2025-01-03 14:41:14 +0700

    controller

--- src/Http/Controllers/AuthApiController.php
+++ src/Http/Controllers/AuthApiController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Edwinrtoha\Laravelboilerplate\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
