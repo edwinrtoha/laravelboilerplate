06c1642f42a4c83572816d349cee50da547e0d79
Edwin Rahmad Toha <edwin.rahmad.toha007@gmail.com>
2025-09-06 14:45:21 +0700

    Models/WorkflowTransition.php

--- src/Models/WorkflowTransition.php
+++ src/Models/WorkflowTransition.php
@@ -3,9 +3,27 @@
 namespace Edwinrtoha\Laravelboilerplate\Models;
 
 use Edwinrtoha\Laravelboilerplate\Models\ModelStd;
+use Spatie\Permission\Models\Permission;
 
 class WorkflowTransition extends ModelStd
 {
+    public function workflow()
+    {
+        return $this->hasOne(Workflow::class, 'id', 'workflow_id');
+    }
+
+    public function permissions()
+    {
+        // many-to-many via workflow_transition_permissions
+        return $this->belongsToMany(
+            Permission::class,
+            'workflow_transition_permissions',
+            'workflow_transition_id', // FK on pivot to this model
+            'permission_id'           // FK on pivot to Permission
+        )->withTimestamps()          // since your pivot has timestamps
+        ->withPivot('id')         // optional if you need the pivot UUID
+        ->as('link');              // optional alias for the pivot relation
+    }
     public function fromState()
     {
         return $this->hasOne(WorkflowState::class, 'id', 'from_state_id');
